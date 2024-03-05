4ac5cc7b4ca254734b0f6dc3833e38497e0e1c6e
AEC-Tech <35284118+AEC-Tech@users.noreply.github.com>
2024-03-05 16:14:56 +0530

    Add files via upload

---
+++ message.php
@@ -0,0 +1,19 @@
+<?php
+$a = 90; // Integer variable to hold marks
+$b = 45.75;
+$c = $a + $b;
+echo var_dump($a);
+echo var_dump($b);
+echo var_dump($c);
+$n = "Anjali"; // String variable to hold name of the user
+echo "<h1>Web Based Programming</h1><br>";
+echo "<h2>Sum of $a and $b is $c</h2>";
+echo "<h2>Thank you $n, for Visiting</h2>";
+echo var_dump($n);
+$ans = true;
+$days = array("Monday","Tuesday","Wednesday");
+echo var_dump($ans);
+echo var_dump($days);
+echo "<BR> $days[0] $days[1] $days[2]";
+echo "<h1>x <sup>2</sup> - 2 = 0</h1>";
+?>
